c9ec8cad84010f89ac6f52dcc9200e068e1f5c30
franc13e <francinefaith03@gmail.com>
2026-09-07 03:55:23 +0800

    Fix CORS for Vercel

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://formly-teal.vercel.app",
+        "https://*.vercel.app",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
