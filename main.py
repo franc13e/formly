555d6459bd2b34719a67ab404e69698feafe14fe
franc13e <francinefaith03@gmail.com>
2026-09-13 23:35:38 +0800

    Fix CORS preflight OPTIONS requests

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["*"],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
+    max_age=3600,
 )
 
 GROQ_KEY = os.getenv("GROQ_API_KEY", "")
